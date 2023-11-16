74aa90a08d1c3e6cc9c9ec6847a54fe41fd1360e
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-16 21:19:15 +0530

    'changes'

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -1,7 +1,14 @@
 package com.tests;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,6 +16,16 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class BaseTest {
 
     protected WebDriver driver;
+
+    protected ExtentReports extent;
+    protected ExtentTest test;
+
+    @BeforeSuite
+    public void setUpSuite() {
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("extent.html");
+        extent = new ExtentReports();
+        extent.attachReporter(sparkReporter);
+    }
 
     @BeforeMethod
     public void setUp() {
@@ -33,5 +50,10 @@ public class BaseTest {
             driver.quit();
         }
         Thread.sleep(5000);
+    }
+
+    @AfterSuite
+    public void tearDownSuite() {
+        extent.flush();
     }
 }
