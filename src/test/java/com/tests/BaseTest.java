351be2562a992dcc5ac17424b11af03f7c8d8888
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-16 20:23:49 +0530

    'changes'

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 
@@ -14,13 +14,16 @@ public class BaseTest {
     public void setUp() {
         // Set the path to the EdgeDriver executable
         // try {
-        //     System.setProperty("webdriver.edge.driver", "/usr/local/bin/msedgedriver");
+        // System.setProperty("webdriver.edge.driver", "/usr/local/bin/msedgedriver");
         // } finally {
-        //     System.setProperty("webdriver.edge.driver", "/src/Resources/msedgedriver.exe");
+        // System.setProperty("webdriver.edge.driver",
+        // "/src/Resources/msedgedriver.exe");
         // }
 
         // Create a new instance of the EdgeDriver
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterMethod
