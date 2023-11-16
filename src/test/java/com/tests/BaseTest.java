becbdf64d46e894cf9abbffb357bdb4b4e42049b
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-16 22:08:56 +0530

    changes

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -1,40 +1,29 @@
 package com.tests;
 
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+// import org.openqa.selenium.edge.EdgeDriver;
+
 
 public class BaseTest {
 
     protected WebDriver driver;
 
-    protected ExtentReports extent;
-    protected ExtentTest test;
-
-    @BeforeSuite
-    public void setUpSuite() {
-        ExtentSparkReporter sparkReporter = new ExtentSparkReporter("/target/extent.html");
-        extent = new ExtentReports();
-        extent.attachReporter(sparkReporter);
-    }
-
+ 
     @BeforeMethod
     public void setUp() {
         // Set the path to the EdgeDriver executable
         // try {
-        // System.setProperty("webdriver.edge.driver", "/usr/local/bin/msedgedriver");
+        //     System.setProperty("webdriver.edge.driver", "/usr/local/bin/msedgedriver");
         // } finally {
-        // System.setProperty("webdriver.edge.driver",
-        // "/src/Resources/msedgedriver.exe");
+        //     System.setProperty("webdriver.edge.driver",
+        //             "src\\Resources\\msedgedriver.exe");
         // }
 
         // Create a new instance of the EdgeDriver
@@ -52,8 +41,5 @@ public class BaseTest {
         Thread.sleep(5000);
     }
 
-    @AfterSuite
-    public void tearDownSuite() {
-        extent.flush();
-    }
+    
 }
