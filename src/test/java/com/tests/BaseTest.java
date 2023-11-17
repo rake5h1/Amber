bd3c1f932cedab1d4bfeddafbeb9836d036e4c8c
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-17 08:37:25 +0530

    changes

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -8,7 +8,6 @@ import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 
 
 public class BaseTest {
