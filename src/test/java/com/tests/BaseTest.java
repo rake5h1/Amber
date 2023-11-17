99afc8543dcea9fae6e08cc28a10d761eb7abb6c
rake5h1 <rakeshkapgate1@gmail.com>
2023-11-17 21:06:50 +0530

    Change

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -2,9 +2,6 @@ package com.tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
